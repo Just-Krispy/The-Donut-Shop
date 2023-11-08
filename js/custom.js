a15187d318659f3800d216166cd2e0363620e15c
Krispy <149973066+Just-Krispy@users.noreply.github.com>
2023-11-08 11:52:51 -0500

    still working

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+const chatContainer = document.getElementById("chat-container");
+const userInput = document.getElementById("user-input");
+
+function appendMessage(message, isAgent) {
+    const messageText = isAgent ? `Dough: ${message}` : `You: ${message}`;
+    const messageParagraph = document.createElement("p");
+    messageParagraph.textContent = messageText;
+    messageParagraph.className = isAgent ? "system-message" : "user-response";
+    chatContainer.insertBefore(messageParagraph, userInput);
+}
+
+function handleUserResponse(response) {
+    response = response.toLowerCase();
+    
+    // Rest of your JavaScript code...
+}
+
+userInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        const response = userInput.value;
+        userInput.value = "";
+        appendMessage(response, false);
+        handleUserResponse(response);
+    }
+});
+
+let userState = '';
+let customerName = '';
+let customerEmail = '';
+let contactNumber = '';
+let optIn;
+let donutType = '';
+let donutQuantity = '';
